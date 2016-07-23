82abe03b2ba59c21124141c32462b2f38c56f659
zhangsan <zhangsan@qq.com>
2016-07-23 16:06:23 +0800

    startActivityForActivity的用法

--- app/src/main/java/com/bbb/aaa/myactivity/MainActivity.java
+++ app/src/main/java/com/bbb/aaa/myactivity/MainActivity.java
@@ -11,7 +11,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    Button btn;
+    public static final String NAME = "name";
+    Button mBtn1,mBtn2;
     EditText nameTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,40 +20,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
         nameTxt = (EditText) this.findViewById(R.id.nameTxt);
-        btn=(Button)this.findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        mBtn1 =(Button)this.findViewById(R.id.btn1);
+        mBtn2 =(Button)this.findViewById(R.id.btn2);
+        mBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Toast.makeText(MainActivity.this, "hello toast", Toast.LENGTH_LONG).show();
                 Intent intent=new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("name",nameTxt.getText().toString());
-                startActivity(intent);
+                intent.putExtra(NAME,nameTxt.getText().toString());
+               //startActivity(intent);
+                startActivityForResult(intent, 10);
+            }
+        });
+        mBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "hello toast", Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra(NAME,nameTxt.getText().toString());
+                //startActivity(intent);
+                startActivityForResult(intent, 20);
             }
         });
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart: ");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       // super.onActivityResult(requestCode, resultCode, data);
+        String msg=data.getStringExtra("back");
+        switch(requestCode){
+            case 10:
+                Toast.makeText(MainActivity.this, msg+"：from SecondActivity", Toast.LENGTH_LONG).show();
+                break;
+            case 20:
+                Toast.makeText(MainActivity.this, msg +": from ThirdActivity", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+
+        Log.d(TAG, "onActivityResult: "+requestCode+","+resultCode);
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume:222 ");
 
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop: ");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause: ");
-    }
 }
--- app/src/main/java/com/bbb/aaa/myactivity/ThirdActivity.java
+++ app/src/main/java/com/bbb/aaa/myactivity/ThirdActivity.java
@@ -1,31 +1,33 @@
 package com.bbb.aaa.myactivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by lsx on 2016/7/22.
  */
-public class SecondActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
     private TextView tv;
     private EditText mEditText;
     Button mBtn;
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_third);
         String msg=getIntent().getStringExtra(MainActivity.NAME);
-        tv = (TextView) this.findViewById(R.id.second_textView);
-        mEditText = (EditText) this.findViewById(R.id.second_editText);
-        mBtn = (Button) this.findViewById(R.id.second_btn);
+        tv = (TextView) this.findViewById(R.id.third_textView);
+        mEditText = (EditText) this.findViewById(R.id.third_editText);
+        mBtn = (Button) this.findViewById(R.id.third_btn);
 
 
         //Toast.makeText(SecondActivity.this, msg, Toast.LENGTH_SHORT).show();
